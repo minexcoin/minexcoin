5807e7bb17a80e241c1760004e858c40146f93cb
Roman Hulenko <rhulenko@minexsystems.com>
2017-09-11 19:00:37 +0300

    MC-17 Fix tests that fail during building

--- src/test/test_bitcoin.cpp
+++ src/test/test_bitcoin.cpp
@@ -127,7 +127,7 @@ TestChain100Setup::CreateAndProcessBlock(const std::vector<CMutableTransaction>&
     unsigned int extraNonce = 0;
     IncrementExtraNonce(&block, chainActive.Tip(), extraNonce);
 
-    while (!CheckProofOfWork(block.GetHash(), block.nBits, chainparams.GetConsensus())) ++block.nNonce;
+    while (!CheckProofOfWork(block.GetHash(), block.nBits, chainparams.GetConsensus())) block.nNonce = ArithToUint256(UintToArith256(block.nNonce) + 1);
 
     std::shared_ptr<const CBlock> shared_pblock = std::make_shared<const CBlock>(block);
     ProcessNewBlock(chainparams, shared_pblock, true, NULL);
